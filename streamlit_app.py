a24a8b16ed5675fa241a7fdb646060b604d16514
emma-carballal <carballal.emma@gmail.com>
2023-10-05 11:04:24 +0200

    Add message

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,8 @@ with st.sidebar:
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
